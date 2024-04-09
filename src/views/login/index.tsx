0f8d3697a7dadf08c6ce3d8861b0881e18872788
JingKun <751051079@qq.com>
2024-04-09 09:36:47 +0800

    1

---
+++ src/views/login/index.tsx
@@ -0,0 +1,17 @@
+import "./index.less";
+
+const Login = () => {
+	return (
+		<div className="login-container">
+			<div className="login-box">
+				<div className="login-form">
+					<div className="login-logo">
+						<span className="logo-text">Hooks-Admin</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
